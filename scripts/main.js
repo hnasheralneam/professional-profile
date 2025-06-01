6f0ae6869127b3f0bd51246b0edf116afa7fcc93
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2025-06-01 15:36:36 -0400

    Add arrows to navigate, update text content

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,26 @@
+let page;
+let sections = ["meet", "leadership", "skills", "clubs", "achievements", "work", "volunteering", "contact"];
+
 // on page load, parse the location to find the page and go to that section
 window.onload = function() {
-   let page = window.location.hash.substring(1);
+   page = window.location.hash.substring(1);
    console.log(page);
    if (page === "") {
       page = "meet";
    }
    goToSection(page);
+   updateArrowVisibility(sections.indexOf(page));
 }
 
-function goToSection(page) {
+function goToSection(newPage) {
+   page = newPage;
    document.location = "#" + page;
 
    let indicator = document.getElementById(page + "Indicator");
    if (document.querySelector("span.active")) document.querySelector("span.active").classList.remove("active");
    indicator.classList.add("active")
+   updateArrowVisibility(sections.indexOf(page));
 }
-
 
 
 // Hover
@@ -37,6 +42,34 @@ function info(THIS) {
 }
 
 document.addEventListener("mousemove", (event) => {
-   dynamHov.style.left = (event.clientX + 18) + "px";
-   dynamHov.style.top = (event.clientY - 5) + "px";
+   dynamHov.style.left = (event.clientX + 20) + "px";
+   dynamHov.style.top = (event.clientY - 12) + "px";
 });
+
+
+// Arrows
+function nextSection() {
+   let currentIndex = sections.indexOf(page);
+   if (currentIndex < sections.length - 1) {
+      page = sections[currentIndex + 1];
+      goToSection(page);
+   }
+}
+
+function lastSection() {
+   let currentIndex = sections.indexOf(page);
+   if (currentIndex > 0) {
+      page = sections[currentIndex - 1];
+      goToSection(page);
+   }
+}
+
+function updateArrowVisibility(currentIndex) {
+   document.querySelector(".arrow-left").classList.remove("hidden");
+   document.querySelector(".arrow-right").classList.remove("hidden");
+
+   if (currentIndex == 0)
+      document.querySelector(".arrow-left").classList.add("hidden");
+   if (currentIndex == sections.length - 1)
+      document.querySelector(".arrow-right").classList.add("hidden");
+}
